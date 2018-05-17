5a551536b4e45a29a538f4275027bf8c53fb5197
Christofer Chan <devchumbing@gmail.com>
2018-05-17 08:47:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!";
 } else if (string.toUpperCase === string) {
     return "YES INDEED"
+} else if (string.toEqual("I love you, Grandma")){
+    return "I love you, too"
   
 }
 }
