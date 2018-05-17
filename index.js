76c1cc942804883dce5b9f9ccc629074019a11d9
Christofer Chan <devchumbing@gmail.com>
2018-05-17 08:44:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function logShout(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string 
+  if (string.toLowerCase() === string){
     return "I can\'t hear you!";
 }
