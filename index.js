962562f9e9e1785ff359b95ab80f60780882c0e6
Christofer Chan <devchumbing@gmail.com>
2018-05-17 08:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,4 @@ function sayHiToGrandma(string) {
 function sayHiToGrandma(string) {
   if(string === "I love you, Grandma") {
     return "I love you, too"
-  }
-  
-}
- 
-function sayHiToGrandma(string) {
-  if(string === "I love you, too") {
-    
-    
-  }
-  
-}
+} else if (string === )
